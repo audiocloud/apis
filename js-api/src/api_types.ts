e8ed485e9d6ea71a3eb844baefd9a22078a1f1cf
Rok <rok.kroflic@gmail.com>
2022-08-03 15:51:52 +0200

    0.0.10 - First version of api_type.ts exports.

---
+++ js-api/src/api_types.ts
@@ -0,0 +1,6 @@
+export { CreateSession, SessionSpec } from "./cloud/apps"
+export { ModifySessionSpec } from "./change"
+export { DomainSessionCommand, WebSocketCommand, WebSocketEvent } from "./domain"
+export { AppId, AppSessionId, SecureKey, SessionId } from "./new_types"
+export { SessionSecurity } from "./session"
+export { JsonTimeStamp } from "./time"
