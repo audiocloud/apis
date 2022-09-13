eb4bd0431e03ae44e7a74d0fd8dee29a37e622d0
Bojan Šernek <bojan@distopiksound.com>
2022-09-13 22:04:37 +0800

    Preparing auto deploy

--- js-api/src/api_types.ts
+++
@@ -1,6 +0,0 @@
-export { CreateSession, SessionSpec } from "./cloud/apps"
-export { ModifySessionSpec } from "./change"
-export { DomainSessionCommand, WebSocketCommand, WebSocketEvent } from "./domain"
-export { AppId, AppSessionId, SecureKey, SessionId } from "./new_types"
-export { SessionSecurity } from "./session"
-export { JsonTimeStamp } from "./time"
--- js-api/src/utils/option.ts
+++
@@ -1,5 +0,0 @@
-import { Static, Type, TSchema } from "@sinclair/typebox";
-
-export default function Option<T extends TSchema>(t: T) {
-    return Type.Union([Type.Null(), t])
-}
